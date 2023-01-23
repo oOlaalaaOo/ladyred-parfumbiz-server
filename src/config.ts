283e00f0b7b24c7959321db219985cd4a4efab08
Benjamin Espanola <benjamin.espanola@magneds.com>
2023-01-23 20:00:40 +0800

    updated db config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB = process.env.MONGO_DB;
+
+const dbConnection = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
 export default {
     portNumber: process.env.PORT_NUMBER || 5000,
-    dbConnection:
-        process.env.DB_CONNECTION || 'mongodb://localhost:27017/ladyred-parfumebiz',
+    dbConnection: dbConnection,
 };
